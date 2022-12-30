adc719c09c80d534f9248130df8332a28c0c9bbc
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-30 12:33:47 -0500

    Break out individual chooseEmployee, getEngineer, getIntern functions for conditional prompts

--- index.js
+++ index.js
@@ -26,41 +26,97 @@ const getManager = () => {
       },
     ])
     .then((manager) => {
-      inquirer.prompt([
+      console.log("add data to array" + manager)
+       chooseEmployee();
+    });
+};
+
+// CHOOSE EMPLOYEE
+
+const chooseEmployee = () => {
+    inquirer
+      .prompt([
         {
           name: "emp_type",
           type: "list",
           message: "What type of employee would you like to add?",
-          choices: ["Engineer", "Intern"],
+          choices: ["Engineer", "Intern", "Finished"],
         },
-        {
-            name: "emp_name",
-            type: "input",
-            message: "What's the employee's name?",
-          },
-          {
-            name: "emp_id",
-            type: "number",
-            message: "What's the employee's id?",
-          },
-          {
-            name: "emp_email",
-            type: "input",
-            message: "What's the employee's email address?",
-          },
-          {
-            name: "eng_github",
-            type: "input",
-            message: "What's the engineer's Github Username?",
-            when: (answers) => answers.emp_type === "Engineer",
-          },
-          {
-            name: "int_school",
-            type: "input",
-            message: "What school does the intern attend?",
-            when: (answers) => answers.emp_type === "Intern",
-          },
-      ]);
+      ])
+      .then((answer) => {
+        if (answer.emp_type === "Finished") {
+          console.log("finished");
+          return;
+        } else if (answer.emp_type === "Engineer") {
+            console.log(answer.emp_type + " has been selected");
+             getEngineer();
+        } 
+        else {
+          console.log(answer.emp_type + " has been selected");
+          getIntern();
+        }
+      });
+  };
+
+// GET ENGINEER
+const getEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        name: "emp_name",
+        type: "input",
+        message: "What's the employee's name?",
+      },
+      {
+        name: "emp_id",
+        type: "number",
+        message: "What's the employee's id?",
+      },
+      {
+        name: "emp_email",
+        type: "input",
+        message: "What's the employee's email address?",
+      },
+      {
+        name: "eng_github",
+        type: "input",
+        message: "What's the engineer's Github Username?",
+      },
+    ])
+    .then((answers) => {
+      console.log("add answer to array and run function" + answers);
+      chooseEmployee();
+    });
+};
+
+// GET INTERN
+const getIntern = () => {
+  inquirer
+    .prompt([
+      {
+        name: "emp_name",
+        type: "input",
+        message: "What's the employee's name?",
+      },
+      {
+        name: "emp_id",
+        type: "number",
+        message: "What's the employee's id?",
+      },
+      {
+        name: "emp_email",
+        type: "input",
+        message: "What's the employee's email address?",
+      },
+      {
+        name: "int_school",
+        type: "input",
+        message: "What school does the intern attend?",
+      },
+    ])
+    .then((answers) => {
+      console.log("add answer to array and run function" + answers);
+      chooseEmployee();
     });
 };
 
